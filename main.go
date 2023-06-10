59fcb56a58975c16730fdc8ff18366133b178552
MAfzal <mohamadafzal06@gmail.com>
2023-06-10 09:08:05 +0330

    development: add lock to store for safe cuncurrent write and read

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"io"
 	"log"
 	"net/http"
+	"sync"
 
 	"github.com/gorilla/mux"
 )
@@ -13,37 +14,51 @@ var (
 	ErrNoSuchKey = errors.New("no such key")
 )
 
-type store map[string]string
+type store struct {
+	m map[string]string
+	l *sync.RWMutex
+}
 
-func New() store {
-	return store{}
+func New(l *sync.RWMutex) store {
+	return store{
+		m: make(map[string]string),
+		l: l,
+	}
 }
 
 func (s store) Put(key, value string) error {
-	s[key] = value
+	s.l.Lock()
+	s.m[key] = value
+	defer s.l.Unlock()
 	return nil
 }
 
 func (s store) Get(key string) (string, error) {
-	value, ok := s[key]
+	s.l.RLock()
+	value, ok := s.m[key]
 	if !ok {
 		return "", ErrNoSuchKey
 	}
+	defer s.l.RUnlock()
 
 	return value, nil
 }
 
 func (s store) Delete(key string) error {
-	delete(s, key)
+	s.l.Lock()
+
+	delete(s.m, key)
+
+	defer s.l.Unlock()
 
 	return nil
 }
 
 type Handler struct {
-	store store
+	store *store
 }
 
-func NewHandler(s store) Handler {
+func NewHandler(s *store) Handler {
 	return Handler{
 		store: s,
 	}
@@ -87,9 +102,9 @@ func (h Handler) keyValueGetHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	s := New()
+	s := New(&sync.RWMutex{})
 	r := mux.NewRouter()
-	h := NewHandler(s)
+	h := NewHandler(&s)
 
 	r.HandleFunc("/v1/{key}", h.keyValuePutHandler).Methods("PUT")
 	r.HandleFunc("/v1/{key}", h.keyValueGetHandler).Methods("GET")
